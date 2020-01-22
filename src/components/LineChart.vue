ab3d1da33b9760f7294aa13f28a610fa6f6c4b5e
Andei Khabarov <akhabarow@gmail.com>
2020-01-22 21:40:54 +0300

    Add simple graphic

---
+++ src/components/LineChart.vue
@@ -0,0 +1,21 @@
+<script>
+import { Line } from 'vue-chartjs';
+
+export default {
+  name: 'LineChart',
+  extends: Line,
+  props: ['chartdata', 'options'],
+  mounted() {
+    this.renderChart({
+      labels: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+      datasets: [
+        {
+          label: 'График вибрации с системы Spectra',
+          backgroundColor: '#f87979',
+          data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11],
+        },
+      ],
+    }, { responsive: true, maintainAspectRatio: false });
+  },
+};
+</script>
--- src/views/Charts.vue
+++ src/views/Charts.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    <h1>Simple vue-chart.js view</h1>
+    <h1>Message</h1>
+    <div id="chart">
+      <LineChart/>
+    </div>
   </div>
 </template>
+
+<script>
+import LineChart from '../components/LineChart.vue';
+
+export default {
+  name: 'charts',
+  components: {
+    LineChart,
+  },
+};
+</script>
+
+<style lang="scss">
+#chart {
+  display: block;
+  width: 80%;
+  margin: 0 auto;
+}
+</style>
