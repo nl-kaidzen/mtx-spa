af6229435efaf95044158a33dbf73a5421cadef5
Andei Khabarov <akhabarow@gmail.com>
2020-01-24 08:35:09 +0300

    Remoove comments

--- src/router/index.js
+++ src/router/index.js
@@ -13,25 +13,16 @@ const routes = [
   {
     path: '/user',
     name: 'user',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/User.vue'),
   },
   {
     path: '/charts',
     name: 'charts',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Charts.vue'),
   },
   {
     path: '/table',
     name: 'table',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Table.vue'),
   },
 ];
